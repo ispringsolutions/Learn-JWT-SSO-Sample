d3510c24c06ea538e0f887d35b47c4beafd5f286
Sergey Shambir <sergey.shambir@ispring.ru>
2020-06-10 13:25:37 +0300

    Re-formatted code.

--- authentication.php
+++ authentication.php
@@ -1,94 +1,94 @@
 ﻿<?php
-   // Include php JWT implementation library
-   // https://github.com/firebase/php-jwt/tree/master/src
 
-   require_once ("../lib/sso/jwt/JWT.php");
-   require_once ("../lib/sso/jwt/BeforeValidException.php");
-   require_once ("../lib/sso/jwt/ExpiredException.php");
-   require_once ("../lib/sso/jwt/SignatureInvalidException.php");
+// Include php JWT implementation library
+// https://github.com/firebase/php-jwt/tree/master/src
 
-   const PROTOCOL_STRING       = 'https://';
-   const ISPRING_LEARN_DOMAIN  = '***.ispringlearn.com';
-   const ISPRING_JWT_LOGIN_URL = '/sso/login/jwt?jwt=';
+require_once('../lib/sso/jwt/JWT.php');
+require_once('../lib/sso/jwt/BeforeValidException.php');
+require_once('../lib/sso/jwt/ExpiredException.php');
+require_once('../lib/sso/jwt/SignatureInvalidException.php');
 
-   const EXAMPLE_USER_EMAIL                    = 'user@mail.com';
-   const EXAMPLE_USER_PASSWORD_ON_YOUR_SERVER  = 'verySecurePassword';
+const PROTOCOL_STRING = 'https://';
+const ISPRING_LEARN_DOMAIN = '***.ispringlearn.com';
+const ISPRING_JWT_LOGIN_URL = '/sso/login/jwt?jwt=';
 
-   // Enter the same secret key here: %Your_iSpring_Learn_domain%/settings/sso/jwt
-   const EXAMPLE_JWT_SECRET_KEY = '********';
-   const EXAMPLE_JWT_ENCODE_ALG = 'HS256';
+const EXAMPLE_USER_EMAIL = 'user@mail.com';
+const EXAMPLE_USER_PASSWORD_ON_YOUR_SERVER = 'verySecurePassword';
 
-   function sendVerifiedUserJWTData($userEmail)
-   {
-       $tokenId    = base64_encode(mcrypt_create_iv(32));
-       $issuedAt   = time();
-       $expire     = $issuedAt + 60;     // Add 60 seconds
+// Enter the same secret key here: %Your_iSpring_Learn_domain%/settings/sso/jwt
+const EXAMPLE_JWT_SECRET_KEY = '********';
+const EXAMPLE_JWT_ENCODE_ALG = 'HS256';
 
-       $data = [
-           'iat'   => $issuedAt,         // Time when the token was generated
-           'jti'   => $tokenId,          // A unique identifier for the token
-           'exp'   => $expire,           // Time to destruct token
-           'email' => $userEmail         // iSpring Learn user's email that you verify
-       ];
+function sendVerifiedUserJWTData($userEmail)
+{
+    $tokenId = base64_encode(mcrypt_create_iv(32));
+    $issuedAt = time();
+    $expire = $issuedAt + 60;     // Add 60 seconds
 
-       $jwt = JWT::encode(
-           $data,                  // Data to be encoded in the JWT
-           EXAMPLE_JWT_SECRET_KEY, // The key for encryption
-           EXAMPLE_JWT_ENCODE_ALG  // Algorithm used to encode
-       );
+    $data = [
+        'iat' => $issuedAt,         // Time when the token was generated
+        'jti' => $tokenId,          // A unique identifier for the token
+        'exp' => $expire,           // Time to destruct token
+        'email' => $userEmail         // iSpring Learn user's email that you verify
+    ];
 
-       // Redirect to iSpring Learn JWT login page
-       $redirectUrl = PROTOCOL_STRING . ISPRING_LEARN_DOMAIN . ISPRING_JWT_LOGIN_URL . $jwt;
-       header("Location: " . $redirectUrl);
-   }
+    $jwt = JWT::encode(
+        $data,                  // Data to be encoded in the JWT
+        EXAMPLE_JWT_SECRET_KEY, // The key for encryption
+        EXAMPLE_JWT_ENCODE_ALG  // Algorithm used to encode
+    );
 
-   // Checks if user is already logged into the system
-   // In this example it just returns false
-   function isUserAlreadyLogged()
-   {
-       return false;
-   }
+    // Redirect to iSpring Learn JWT login page
+    $redirectUrl = PROTOCOL_STRING . ISPRING_LEARN_DOMAIN . ISPRING_JWT_LOGIN_URL . $jwt;
+    header('Location: ' . $redirectUrl);
+}
 
-   // Get data from your login form.
-   // In this example we return example strings
-   function getEmailFormField()
-   {
-       return EXAMPLE_USER_EMAIL;
-   }
+// Checks if user is already logged into the system
+// In this example it just returns false
+function isUserAlreadyLogged()
+{
+    return false;
+}
 
-   function getPasswordFormField()
-   {
-       return EXAMPLE_USER_PASSWORD_ON_YOUR_SERVER;
-   }
+// Get data from your login form.
+// In this example we return example strings
+function getEmailFormField()
+{
+    return EXAMPLE_USER_EMAIL;
+}
 
-   // Check if a user exists in your system. Connect to your database and check if the user exists and the password is valid   
+function getPasswordFormField()
+{
+    return EXAMPLE_USER_PASSWORD_ON_YOUR_SERVER;
+}
+
+// Check if a user exists in your system. Connect to your database and check if the user exists and the password is valid
 function isUserExistsAndPasswordValid($userEmail, $userPassword)
-   {
-       return ($userEmail == EXAMPLE_USER_EMAIL) &&
-     ($userPassword == EXAMPLE_USER_PASSWORD_ON_YOUR_SERVER);
-   }
+{
+    return ($userEmail === EXAMPLE_USER_EMAIL) && ($userPassword === EXAMPLE_USER_PASSWORD_ON_YOUR_SERVER);
+}
 
-   function processJWTAuthorization()
-   {
-       if (isUserAlreadyLogged())
-       {
-           // If your user is already logged into your system, you can get his email from session or etc.
-           sendVerifiedUserJWTData(EXAMPLE_USER_EMAIL);
-       }
-       else  // User should log on to your system. Prompt user to enter an email and password
-       {
-           $userEmail = getEmailFormField();
-           $userPassword = getPasswordFormField();
-           if (isUserExistsAndPasswordValid($userEmail, $userPassword))
-           {
-               sendVerifiedUserJWTData($userEmail);
-               return;
-           }
-       }
-       // If that user doesn't exist, send a 401 Error
-       header('HTTP/1.0 401 Unauthorized');
-       exit(0);
-   }
+function processJWTAuthorization()
+{
+    if (isUserAlreadyLogged())
+    {
+        // If your user is already logged into your system, you can get his email from session or etc.
+        sendVerifiedUserJWTData(EXAMPLE_USER_EMAIL);
+    }
+    else  // User should log on to your system. Prompt user to enter an email and password
+    {
+        $userEmail = getEmailFormField();
+        $userPassword = getPasswordFormField();
+        if (isUserExistsAndPasswordValid($userEmail, $userPassword))
+        {
+            sendVerifiedUserJWTData($userEmail);
+            return;
+        }
+    }
+    // If that user doesn't exist, send a 401 Error
+    header('HTTP/1.0 401 Unauthorized');
+    exit(0);
+}
 
-   // Let's start!
-   processJWTAuthorization();
+// Let's start!
+processJWTAuthorization();
--- logout.php
+++ logout.php
@@ -1,26 +1,27 @@
 ﻿<?php
-   // Include php JWT implementation library
-   // https://github.com/firebase/php-jwt/tree/master/src
 
-   require_once ("../lib/sso/jwt/JWT.php");
-   require_once ("../lib/sso/jwt/BeforeValidException.php");
-   require_once ("../lib/sso/jwt/ExpiredException.php");
-   require_once ("../lib/sso/jwt/SignatureInvalidException.php");
+// Include php JWT implementation library
+// https://github.com/firebase/php-jwt/tree/master/src
 
-   // Enter the same secret key here: %Your_iSpring_Learn_domain%/settings/sso/jwt
-   const EXAMPLE_JWT_SECRET_KEY                = '********';
-   const EXAMPLE_JWT_ENCODE_ALG                = 'HS256';
+require_once('../lib/sso/jwt/JWT.php');
+require_once('../lib/sso/jwt/BeforeValidException.php');
+require_once('../lib/sso/jwt/ExpiredException.php');
+require_once('../lib/sso/jwt/SignatureInvalidException.php');
 
-   $jwtString = $_GET['jwt'];
-   try
-   {
-       $token = JWT::decode($jwtString, EXAMPLE_JWT_SECRET_KEY, EXAMPLE_JWT_ENCODE_ALG);
-       $email = isset($token->email) ? $token->email : '';
-       print("User with email {$email} is trying to logout!");
-   }
-   catch (Exception $e)
-   {
-       // If that user doesn't exist, send a 401 Error
-       header('HTTP/1.0 401 Unauthorized');
-       exit(0);
-   }
+// Enter the same secret key here: %Your_iSpring_Learn_domain%/settings/sso/jwt
+const EXAMPLE_JWT_SECRET_KEY = '********';
+const EXAMPLE_JWT_ENCODE_ALG = 'HS256';
+
+$jwtString = $_GET['jwt'];
+try
+{
+    $token = JWT::decode($jwtString, EXAMPLE_JWT_SECRET_KEY, EXAMPLE_JWT_ENCODE_ALG);
+    $email = isset($token->email) ? $token->email : '';
+    print("User with email {$email} is trying to logout!");
+}
+catch (Exception $e)
+{
+    // If that user doesn't exist, send a 401 Error
+    header('HTTP/1.0 401 Unauthorized');
+    exit(0);
+}
